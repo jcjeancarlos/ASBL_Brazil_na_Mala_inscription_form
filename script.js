6a5b36ce7405f3bb0c643c464b73dab13435f70b
jcjeancarlos <jcjeancarlos.1@gmail.com>
2026-03-22 19:45:34 +0100

    new fetch link added

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelector(".form").addEventListener("submit", async function(e) {
         langue: document.getElementById("langue").value
     };
 
-    const response = await fetch("https://script.google.com/macros/s/AKfycbyIX-mlm07TSGH1iXSA7-BHv8CsB0s-8O820qss5n8hnVjtMycWW1f-fhHC9deIPS9j/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycby43uAneM4AJiTldAUcU5_0wRrsstXGlfi7WD0NzLylyPZsWOA-eFNstaoD4-eSZkT-/exec", {
         method: "POST",
         body: JSON.stringify(data)
     });
